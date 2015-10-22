42e60b4f9a3903645e799f020e0f8a08c06b0dfb
JesGor <jesusgorillo@gmail.com>
2015-10-22 13:55:09 +0200

    Terminadas las funcionalidades

--- web/app/urls.py
+++ web/app/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'^eliminar_empresa$', views.eliminar_empresa, name='eliminar_empresa'),
     url(r'^eliminar_empresa_seleccionada$', views.eliminar_empresa_seleccionada, name='eliminar_empresa_seleccionada'),
     url(r'^(?P<empresa_id>[0-9]+)/calificaciones/$', views.calificaciones, name='calificaciones'),
+    url(r'^crear_calificacion$', views.crear_calificacion, name='crear_calificacion'),
+    url(r'^crear_calificacion/nueva_calificacion$', views.nueva_calificacion, name='nueva_calificacion'),
+    url(r'^(?P<empresa_id>[0-9]+)/eliminar_calificacion$', views.eliminar_calificacion, name='eliminar_calificacion'),
+    url(r'^(?P<empresa_id>[0-9]+)/eliminar_calificacion_seleccionada$', views.eliminar_calificacion_seleccionada, name='eliminar_calificacion_seleccionada'),
 ]
